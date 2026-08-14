f12adbb831fc13685f4cdaa5273d49b3078cbc1e
Mohd Muzahid Raza <razamuzahid143@gmail.com>
2026-08-14 22:04:00 +0530

    Time: 15 ms (23.86%), Space: 46.7 MB (9.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,24 @@
 class Solution {
-    static HashMap<Character, Integer> makeFreqMap(String str){
-    HashMap<Character, Integer> mp = new HashMap<>();
-    for(int i = 0; i < str.length(); i++){
-        Character ch = str.charAt(i);
-        if(!mp.containsKey(ch)){
-            mp.put(ch, 1);
-        }
-        else{
-            int currFreq = mp.get(ch);
-            mp.put(ch, currFreq+1);
-        }
-    }
-    return mp;
-    }
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()){
             return false;
         }
-        HashMap<Character, Integer> mp1 = makeFreqMap(s);
-        HashMap<Character, Integer> mp2 = makeFreqMap(t);
-        return mp1.equals(mp2);
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char ch : s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(char ch : t.toCharArray()){
+            if (!map.containsKey(ch)) {
+                return false;
+            }
+            map.put(ch, map.get(ch) - 1);
+            if(map.get(ch) < 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }

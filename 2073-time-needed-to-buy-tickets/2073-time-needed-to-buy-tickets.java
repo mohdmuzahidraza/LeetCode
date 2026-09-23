24528e8377abdfd435ce4450ddd4ed9ffbf7c175
Mohd Muzahid Raza <razamuzahid143@gmail.com>
2026-09-23 22:21:17 +0530

    Time: 0 ms (100%), Space: 43.2 MB (43.76%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,7 +1,8 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int time = 0;
-        for(int i = 0; i < tickets.length; i++){
+        int n = tickets.length;
+        for(int i = 0; i < n; i++){
             if( i <= k){
                 time += Math.min(tickets[i], tickets[k]);
             }
